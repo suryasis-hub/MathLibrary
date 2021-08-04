7ddc62749fae073d5e7e4d45a78d9dd387472a70
Suryasis Paul <suryasispaul@microsoft.com>
2021-08-04 19:33:45 +0530

    Adding code for memoizing known factorials

--- MathLibrary/MathLibrary.cpp
+++ MathLibrary/MathLibrary.cpp
@@ -3,13 +3,22 @@
 
 #include "pch.h"
 #include "framework.h"
+#include <vector>
 #include <iostream>
+
 
 // TODO: This is an example of a library function
 namespace Combinatorics
 {
+	const std::vector<unsigned long long> representableFactors = {1,1,2,6,24,120,720,5040,40320,362880,3628800,39916800,
+		479001600,6227020800,87178291200,1307674368000,20922789888000,355687428096000,6402373705728000,121645100408832000,2432902008176640000};
+
 	unsigned long long factorial(unsigned int n) 
 	{
+		if (representableFactors.size() >= (n-1))
+		{
+			return representableFactors[n];
+		}
 		unsigned long long fact = 1;
 		for (int i = 1; i <= n; i++)
 		{
@@ -21,4 +30,5 @@ namespace Combinatorics
 		}
 		return fact;
 	}
+
 }
--- MathLibraryTest/test.cpp
+++ MathLibraryTest/test.cpp
@@ -7,6 +7,7 @@ TEST(Combinatorial_Factorial, small_ints)
     EXPECT_EQ(Combinatorics::factorial(0), 1);
     EXPECT_EQ(Combinatorics::factorial(1), 1);
     EXPECT_EQ(Combinatorics::factorial(5), 120);
+    EXPECT_EQ(Combinatorics::factorial(20), 2432902008176640000);
 }
 
 TEST(Combinatorial_Factorial, too_big)
